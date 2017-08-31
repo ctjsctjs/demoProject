2c3db149056d136781056d727f036bd282c0b05b
ctjsctjs <ctjsctjs@gmail.com>
2017-08-31 16:59:29 +0800

    3108

--- src/app/js/controllers/mainController.js
+++ src/app/js/controllers/mainController.js
@@ -1,3 +1,71 @@
 app.controller('mainController', ['$scope', function($scope){
-  
+  $scope.query = "";
+  $scope.list  = {
+    "items":[
+      {
+        "title" : "WORKATO",
+        "tags" : ["UI/UX", "HTML5", "CSS3", "Javscript", "jQuery"],
+        "mask" : "true",
+        "link": "https://www.support.workato.com",
+        "style" : {"background-image" : "url(/images/item-workato.svg)",},     
+        "bgColor" : {"background-color" : "rgba(39,152,159, 1)"}
+      },
+      {
+        "title" : "CTJSCTJS",
+        "tags" : ["UI/UX", "HTML5", "CSS3", "jQuery", "Angular"],
+        "mask" : 'false',
+        "link": "http://www.ctjsctjs.com",
+        "style" : {"background-image" : "url(/images/item-ctjsctjs.svg)",},
+        "bgColor" : {"background-color" : "rgba(0,0,0,1)"}
+      },
+      {
+        "title" : "ANGULAR",
+        "tags" : ["Angular"],
+        "mask" : 'true',
+        "link": "/table",
+        "style" : {"background-image" : "url(/images/item-angular.svg)",},
+        "bgColor" : {"background-color" : "rgba(250,152,81,1)"}
+      },
+      {
+        "title" : "CTJSCTJS",
+        "tags" : ["UI/UX", "HTML5", "CSS3", "jQuery", "Angular"],
+        "mask" : 'false',
+        "link": "/table",
+        "style" : { "background-image" : "url(/images/item-ctjsctjs.svg)",},
+        "bgColor" : { "background-color" : "rgba(0,0,0,1)"}
+      },
+      {
+        "title" : "CTJSCTJS",
+        "tags" : ["UI/UX", "HTML5", "CSS3", "jQuery", "Angular"],
+        "mask" : 'false',
+        "link": "/table",
+        "style" : { "background-image" : "url(/images/item-ctjsctjs.svg)",},
+        "bgColor" : { "background-color" : "rgba(0,0,0,1)"}
+      },
+      {
+        "title" : "CTJSCTJS",
+        "tags" : ["UI/UX", "HTML5", "CSS3", "jQuery", "Angular"],
+        "mask" : 'false',
+        "link": "/table",
+        "style" : { "background-image" : "url(/images/item-ctjsctjs.svg)",},
+        "bgColor" : { "background-color" : "rgba(0,0,0,1)"}
+      },
+      {
+        "title" : "CTJSCTJS",
+        "tags" : ["UI/UX", "HTML5", "CSS3", "jQuery", "Angular"],
+        "mask" : 'false',
+        "link": "/table",
+        "style" : { "background-image" : "url(/images/item-ctjsctjs.svg)",},
+        "bgColor" : { "background-color" : "rgba(0,0,0,1)"}
+      },
+      {
+        "title" : "CTJSCTJS",
+        "tags" : ["UI/UX", "HTML5", "CSS3", "jQuery", "Angular"],
+        "mask" : 'false',
+        "link": "/table",
+        "style" : { "background-image" : "url(/images/item-ctjsctjs.svg)",},
+        "bgColor" : { "background-color" : "rgba(0,0,0,1)"}
+      }
+    ]
+  };
 }]);
